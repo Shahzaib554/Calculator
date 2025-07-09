aa0aacbaf9b4a88b0c6dd5f29eb20b813e924715
Shahzaib554 <86713644+Shahzaib554@users.noreply.github.com>
2025-07-09 08:01:02 -0700

    NexusCalc React App Created

--- App.tsx
+++ App.tsx
@@ -1,28 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View style={styles.wrapper}>
+      <Text>Hello</Text>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-  },
+    backgroundColor: 'white'
+  }
 });
 
 export default App;
